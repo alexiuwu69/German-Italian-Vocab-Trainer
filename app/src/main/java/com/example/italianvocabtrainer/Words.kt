31b3f2fb6e1bbd167f07aae4c5f669a58bd360ea
alexiuwu <Alexander.Boosmann@gmail.com>
2024-09-03 19:22:28 +0200

    example wordlist (definitely not the ones for a vocab test)

--- app/src/main/java/com/example/italianvocabtrainer/Words.kt
+++ app/src/main/java/com/example/italianvocabtrainer/Words.kt
@@ -6,13 +6,32 @@ data class Word(val german: String, val italian: String) {
 
 fun getWords(): List<Word> {
     val words = mutableListOf(
-        Word("Hallo", "Buongiorno"),
-        Word("Das Segelboot", "La barca la vela"),
-        Word("hungrig", "affamato"),
-        Word("bedeuten", "significare"),
-        Word("warum", "come mai"),
-        Word("trinkbar", "potabile"),
-        Word("hilfsbereit", "aiutevole")
+        Word("die Abschiebung", "l'espulsione (f.)"),
+        Word("die Angst", "la paura"),
+        Word("die Arbeit", "il lavoro"),
+        Word("die Armut", "la povertà"),
+        Word("das Asyl", "l'asilo (politico)"),
+        Word("die Aufenthaltserlaubnis", "il permesso di soggiorno"),
+        Word("die Ausländerfeindlichkeit", "la xenofobia"),
+        Word("die Aussichtslosigkeit", "la situazione disperata"),
+        Word("die Behörde", "l'autorità"),
+        Word("der Bürgerkrieg", "la guerra civile"),
+        Word("die Diskriminierung", "la discriminazione"),
+        Word("der Durst", "la sete"),
+        Word("die Einwanderungspolitik", "la politica sull'immigrazione"),
+        Word("das Elend", "la miseria"),
+        Word("die Entwurzelung", "lo sradicamento"),
+        Word("ertrinken", "affogare; annegare"),
+        Word("farbig", "colorato, -a"),
+        Word("das Festland", "la terraferma"),
+        Word("die Festnahme", "l'arresto"),
+        Word("fliehen (vor)", "fuggire (da)"),
+        Word("die Flucht", "la fuga"),
+        Word("der Flüchtling", "il rifugiato"),
+        Word("die Gefahr", "il pericolo; il rischio"),
+        Word("gefährlich", "pericoloso, -a"),
+        Word("das Gesetz", "la legge"),
+        Word("die Gesetzesmissachtung", "la disubbidienza alla legge")
     )
     // TODO: More words
     return words
